f7c6004e048356374d5bd0ec19c061ce02641cbd
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-04-06 20:25:14 +0530

    did some changes in view file

--- dataprac/views.py
+++ dataprac/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homeView(req):
-    my_list = [1,2,3,4,"5 is the number with string format",6.66,70,800,-99,100]
+    my_list = [1,2,3,4,"5 is the number with string format",6.66,70,800,-99,1000]
     my_list.append([56,998])
     my_list.extend([56,998])
     return HttpResponse(my_list)
