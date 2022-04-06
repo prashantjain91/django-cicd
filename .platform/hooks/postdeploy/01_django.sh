32e15a4fdec3d7532a73598ff3600f502dac1134
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-04-06 20:50:07 +0530

    changes in .sh file

--- .platform/hooks/postdeploy/01_django.sh
+++ .platform/hooks/postdeploy/01_django.sh
@@ -3,10 +3,10 @@
 source /var/app/venv/*/bin/activate && {
 
 # collecting static files
-python manage.py collectstatic --noinput;
+python3 manage.py collectstatic --noinput;
 # log which migrations have already been applied
-python manage.py showmigrations;
+python3 manage.py showmigrations;
 # migrate the rest
-python manage.py migrate --noinput;
+python3 manage.py migrate --noinput;
 # another command to create a superuser (write your own)
 }
