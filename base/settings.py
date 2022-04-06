3c7589cc75aad7b22902af1aa5954cef62551287
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-04-06 21:51:58 +0530

    Did changes in packages.config file

--- base/settings.py
+++ base/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
     
 }
